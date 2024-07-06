e8124eabc26a08d38524de5130c05f89ea5c9cd0
NoothangiBharathi <noothangi.bharathi1999@gmail.com>
2024-07-06 14:03:04 +0530

    collapsebox

---
+++ script.js
@@ -0,0 +1,12 @@
+ const accordion = document.getElementsByClassName("contentBx");
+
+ for(i = 0; i < accordion.length; i++){
+
+  accordion[i].addEventListener('click',function(){
+    this.classList.toggle('active')
+  })
+ }
+
+
+
+ 
